a24d956f92bab391e34e9185ddafb74db92a17e8
Sakshi <sthombre170@gmail.com>
2025-08-03 02:31:21 +0530

    initial commit

---
+++ src/test/java/com/sakshi/authProject/AuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakshi.authProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
